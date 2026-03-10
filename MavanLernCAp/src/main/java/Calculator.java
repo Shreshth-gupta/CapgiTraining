5fb70512a8c9cb9dc2e8f6c3ae3e42395fc469c8
Shreshtha Gupta <140345011+Shreshth-gupta@users.noreply.github.com>
2026-03-10 12:00:01 +0530

    Add files via upload

---
+++ MavanLernCAp/src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public int calculate(int a,int b){
+        return a+b;
+    }
+    public boolean isPrime(int num){
+        if(num<=1)
+            return false;
+        for (int i = 2; i < num; i++) {
+            if(num%i==0)
+                return false;
+
+        }
+        return true;
+    }
+}
---
+++ MavanLernCAp/src/main/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
+public class CalculatorTest {
+    @Test
+    public void calTest(){
+        Calculator cal=new Calculator();
+        assertEquals(10,cal.calculate(7,3));
+    }
+    @Test
+    public void isPrimeTest(){
+        Calculator cal=new Calculator();
+        assertTrue(cal.isPrime(13));
+        assertFalse(cal.isPrime(8));
+        assertFalse(cal.isPrime(0));
+    }
+}
